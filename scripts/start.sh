141aa1365cc2e29e8a75a8220c1ee504a19fbe8f
Mahdi Jafaree <mahdi-jafaree@Mahdis-MacBook-Pro.local>
2026-04-30 15:59:31 +0500

    fix logging

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 concurrently -k --prefix "{time} | {name} |" -t "HH:mm:ss.SSS" \
-  -n "backend ,admin   " \
+  -n "backend    ,frontend   " \
   -c "blue,yellow,green" \
   "pnpm --filter=backend dev" \
-  "pnpm --filter=admin dev"
+  "pnpm --filter=frontend dev"
